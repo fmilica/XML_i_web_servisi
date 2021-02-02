75f73d0e7da5422c22b0d8eff2f29fd79b30bf79
evaj10 <evaj.evadesign@gmail.com>
2021-02-02 23:01:46 +0100

    Interseptori i gardovi poverenik frontend

--- servisi/poverenik/frontend/src/app/app.module.ts
+++ servisi/poverenik/frontend/src/app/app.module.ts
@@ -1,33 +1,34 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login-register/login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatTabsModule } from '@angular/material/tabs';
-import {MatTooltipModule} from '@angular/material/tooltip';
-
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { ToastrModule } from 'ngx-toastr';
-import { RegisterComponent } from './components/login-register/register/register.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LoginRegisterComponent } from './components/login-register/login-register.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ResenjaGradjaninComponent } from './components/gradjanin/resenja-gradjanin/resenja-gradjanin.component';
 import { ZalbaCutanjeComponent } from './components/gradjanin/zalba-cutanje/zalba-cutanje.component';
 import { ZalbaOdlukaComponent } from './components/gradjanin/zalba-odluka/zalba-odluka.component';
-import { ResenjeComponent } from './components/poverenik/resenje/resenje.component';
 import { ZalbeCutanjeGradjaninComponent } from './components/gradjanin/zalbe-cutanje-gradjanin/zalbe-cutanje-gradjanin.component';
 import { ZalbeOdlukuGradjaninComponent } from './components/gradjanin/zalbe-odluku-gradjanin/zalbe-odluku-gradjanin.component';
-import { ZalbeOdlukuPoverenikComponent } from './components/poverenik/zalbe-odluku-poverenik/zalbe-odluku-poverenik.component';
-import { ZalbeCutanjePoverenikComponent } from './components/poverenik/zalbe-cutanje-poverenik/zalbe-cutanje-poverenik.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginRegisterComponent } from './components/login-register/login-register.component';
+import { LoginComponent } from './components/login-register/login/login.component';
+import { RegisterComponent } from './components/login-register/register/register.component';
 import { ResenjaPoverenikComponent } from './components/poverenik/resenja-poverenik/resenja-poverenik.component';
-import { ResenjaGradjaninComponent } from './components/gradjanin/resenja-gradjanin/resenja-gradjanin.component';
+import { ResenjeComponent } from './components/poverenik/resenje/resenje.component';
+import { ZalbeCutanjePoverenikComponent } from './components/poverenik/zalbe-cutanje-poverenik/zalbe-cutanje-poverenik.component';
+import { ZalbeOdlukuPoverenikComponent } from './components/poverenik/zalbe-odluku-poverenik/zalbe-odluku-poverenik.component';
+import { AuthInterceptorService } from './interceptors/auth-interceptor.service';
+
+
 
 @NgModule({
   declarations: [
@@ -63,7 +64,14 @@ import { ResenjaGradjaninComponent } from './components/gradjanin/resenja-gradja
     MatTooltipModule,
     ToastrModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      // ako multi nije true ovo bi bio jedini interceptor i pregazio bi sve defaultne interceptore
+      multi: true,
+      }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
