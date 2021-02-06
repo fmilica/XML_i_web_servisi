db3dd970cf0dd45566f206958ca8fb749dc320c3
Igor Lukic <igi.l.1999@gmail.com>
2021-02-06 14:53:16 +0100

    Vratili na rpc

--- servisi/organ-vlasti/backend/src/main/java/com/xml/portal/organvlasti/service/soap/endpoint/ZalbaOdbijenEndpointConfig.java
+++ servisi/organ-vlasti/backend/src/main/java/com/xml/portal/organvlasti/service/soap/endpoint/ZalbaOdbijenEndpointConfig.java
@@ -1,7 +1,6 @@
 package com.xml.portal.organvlasti.service.soap.endpoint;
 
 import javax.xml.ws.Endpoint;
-
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +13,10 @@ import com.xml.portal.organvlasti.service.soap.zalba_odbijanje.ZalbaOdbijenServi
 public class ZalbaOdbijenEndpointConfig {
 	@Autowired
 	private Bus bus;
-	
+
 	@Autowired
 	ZalbaOdbijenServiceSoapBindingImpl zalbaOdbijenServiceSoapBindingImpl;
-	
+
 	@Bean(name = "zalbaOdbijenEndpointBean")
 	public Endpoint zalbaOdbijenEndpoint() {
 		EndpointImpl endpoint = new EndpointImpl(bus, zalbaOdbijenServiceSoapBindingImpl);
